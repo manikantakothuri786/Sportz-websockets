cc57657aa17b25c75805ad47899614113d934dee
kothuri manikanta <kothurimanikanta786@gmail.com>
2026-03-18 09:09:56 -0500

    integrated arcjet for securiy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import 'dotenv/config';
 import express from  "express";
 import { matchRouter } from './routes/matches.js';
 import http from 'http';
 import { attachWebSocketServer } from './ws/server.js';
+import { securityMiddleware } from './arcjet.js';
+
 
 const PORT = Number(process.env.PORT || 8000);
 const HOST = process.env.HOST || '0.0.0.0';
@@ -15,6 +18,9 @@ app.use(express.json());
 app.get('/', (req,res) => {
     res.send('Hello from Express Server !');
 });
+
+app.use(securityMiddleware());
+
 
 app.use('/matches', matchRouter);
 
